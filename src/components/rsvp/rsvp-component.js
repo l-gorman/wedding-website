d98f7446caabcfa4e97ac4c70676460162e343f8
l-gorman <lgorman@turing.ac.uk>
2022-09-02 18:07:24 +0100

    functioning RSVP form with google sheets

--- src/components/rsvp/rsvp-component.js
+++ src/components/rsvp/rsvp-component.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { Form, Row, Col, Button} from "react-bootstrap";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { GoogleSpreadsheet } from "google-spreadsheet";
 
 
 function Submit(props){
+    const SPREADSHEET_ID = process.env.REACT_APP_SPREADSHEET_ID;
+    const SHEET_ID = process.env.REACT_APP_SHEET_ID;
+    const CLIENT_EMAIL = process.env.REACT_APP_EMAIL;
+    const PRIVATE_KEY = process.env.REACT_APP_PRIVATE_KEY
+    // process.env.PRIVATE_KEY.replace(/\n/g, '\n')
+    //.replace('/\\n/g', '\n');
+
+    console.log(PRIVATE_KEY)
+    const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+
 
     console.log([
-        props.attendance,
+        props.attending,
         props.firstName,
         props.surname,
         props.email,
@@ -16,36 +27,52 @@ function Submit(props){
     ]
     )
 
+    
+
+    const appendSpreadsheet = async (row) => {
+        try {
+          await doc.useServiceAccountAuth({
+            client_email: CLIENT_EMAIL,
+            private_key: PRIVATE_KEY,
+          });
+          // loads document properties and worksheets
+          await doc.loadInfo();
+      
+          const sheet = doc.sheetsById[SHEET_ID];
+          const result = await sheet.addRow(row);
+
+          console.log(result)
+        } catch (e) {
+          console.error('Error: ', e);
+        }
+      };
+      
+      const newRow = { 
+        attending: props.attending, 
+        firstName: props.firstName,
+        lastName: props.surname,
+        email: props.email,
+        dietaryRequirements:props.dietaryRequirements
+     };
+      
+      appendSpreadsheet(newRow);
+
+
 }
 
 
 
 export default function RSVP(){
 
-    let [attendance, setAttendace] = useState(null)
+    
+
+    let [attending, setaAttending] = useState(null)
     let [firstName, setFirstName] = useState(null)
     let [surname, setSurname] = useState(null)
 
     let [email, setEmail] = useState(null)
 
     let [dietaryRequirements, setDietaryRequirements] = useState(null)
-
-    useEffect(() => {
-      console.log(attendance)
-    
-      
-    }, [attendance])
-
-    useEffect(() => {
-        console.log(firstName)
-      
-        
-      }, [firstName])
-    
-
-
-
-
 
 
     return(
@@ -57,10 +84,10 @@ export default function RSVP(){
         <Form>
             <Form.Group className="mb-3">
             <Form.Check inline type="radio" name="horizontalRadio" label="I can make it" onClick={()=>{
-                setAttendace(true)
+                setaAttending('Yes')
             }} />
             <Form.Check inline type="radio" name="horizontalRadio" label="I can't I'm a loser" onClick={()=>{
-                setAttendace(false)
+                setaAttending('No')
             }}/>
             </Form.Group>
         <Row>
@@ -111,7 +138,7 @@ export default function RSVP(){
 
 <Button className="bg-dark border-0" onClick={()=>{
     Submit({
-        attendance:attendance,
+        attending:attending,
         firstName:firstName,
         surname:surname,
         email:email,
